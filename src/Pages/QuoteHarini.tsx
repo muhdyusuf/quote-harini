ad0db885c9fb4e13ac97c2acd3a34f3281e1ad6d
muhdyusuf2 <143307034+muhdyusuf2@users.noreply.github.com>
2023-09-14 17:35:50 +0800

    add loading state to page

--- src/Pages/QuoteHarini.tsx
+++ src/Pages/QuoteHarini.tsx
@@ -1,10 +1,14 @@
 // import { useAuth } from '@/Context/authContext'
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
 // import { Skeleton } from '@/components/ui/skeleton'
 import { db } from '@/lib/firebaseConfig'
+import { error } from 'console'
 import { Timestamp, collection, getDocs, query, where } from 'firebase/firestore'
 import { Quote } from 'lucide-react'
 import { FC, useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
 
 interface QuoteHariniProps {
   
@@ -13,11 +17,14 @@ interface QuoteHariniProps {
 interface IQuote{
     id:String,
     quote:String,
-    author?:String
+    author?:String,
+    isEmpty?:Boolean
 }
 
 const QuoteHarini: FC<QuoteHariniProps> = ({}) => {
     useEffect(()=>{
+
+        setIsLoading(true)
         const date=new Date()
         const start=date.setHours(0,0,0,0)
         const end=date.setHours(11,59,59,59)
@@ -29,9 +36,17 @@ const QuoteHarini: FC<QuoteHariniProps> = ({}) => {
             ,where("date",">=",startTimeStamp)
             ,where("date","<=",endTimeStamp)
             )
+
+        
     
         getDocs(q).then(querySnapshot=>{
-          
+            if(querySnapshot.empty){
+                setQuote({
+                    id:"",
+                    quote:"",
+                    isEmpty:true
+                })
+            }
             console.log(querySnapshot)
             querySnapshot.forEach(doc=>{
                 const {quote,author}=doc.data()
@@ -41,14 +56,17 @@ const QuoteHarini: FC<QuoteHariniProps> = ({}) => {
                     author
                 })
             })
+            setIsLoading(false)
 
 
+        }).catch(error=>{
+            setIsLoading(false)
         })
     },[])
 
     // const {currentUser}=useAuth()
     const [quote,setQuote]=useState<IQuote|null>(null)
-    // const [isLoading,setIsLoading]=useState<boolean>(false)
+    const [isLoading,setIsLoading]=useState<boolean>(false)
     
     
   return (
@@ -59,6 +77,12 @@ const QuoteHarini: FC<QuoteHariniProps> = ({}) => {
         <Card
             className='max-w-[500px] p-8'
         >
+            {isLoading&&(
+                <Skeleton
+                    className='w-64 aspect-[3/2]'
+                />
+            )}
+            {!isLoading&&(
             <CardContent
                 className='p-0'
             >
@@ -69,8 +93,17 @@ const QuoteHarini: FC<QuoteHariniProps> = ({}) => {
                     className='flex gap-2'
                 >
                     <Quote className='rotate-180 text-8xl text-slate-400/80 min-w-min'/>
+                    {quote&&quote.isEmpty?(
                     <blockquote
                         className='text-5xl text-blue-500 font-black text-left p-0 m-0 leading-1'
+                    >
+                        Tiada Quote 
+                        <br/>
+                       untuk harini
+
+                    </blockquote>
+                    ):(
+                    <blockquote
                         >
                         {quote?.quote.split('\n').map((line,index)=>(
                             <span 
@@ -82,20 +115,29 @@ const QuoteHarini: FC<QuoteHariniProps> = ({}) => {
                             </span>
                         ))}
 
-                    </blockquote>
+                    </blockquote>)}
                 </div>
 
       
-            </CardContent>
+            </CardContent>)}
 
             <CardFooter
-                 className='flex justify-end p-0 pt-2'
+                 className='flex justify-end p-0 pt-4'
             >
+                {quote&&quote.isEmpty?(
+                <Button
+                    asChild
+                >
+                    <Link to={"/addQuote"}>
+                        post a quote
+                    </Link>
+                </Button>
+                ):(
                 <h2
                     className='text-muted-foreground'
                 >
                        - {quote?.author}. 
-                </h2>
+                </h2>)}
             </CardFooter>
         </Card>
         </div>
